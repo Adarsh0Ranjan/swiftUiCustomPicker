c47427cc1ec4c505842259a6ca10c0c2cdb757da
Adarsh Ranjan <104624542+Adarsh0Ranjan@users.noreply.github.com>
2024-01-17 12:43:17 +0530

    first commit

--- SwiftUICustomPicker/ContentView.swift
+++ SwiftUICustomPicker/ContentView.swift
@@ -6,19 +6,95 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
+    @State private var selectedArray = [
+        ["1", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1", "2", "3", "4", "5"],
+    ]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        CustomPicker(dataArrays: $selectedArray)
     }
 }
 
-#Preview {
-    ContentView()
+struct CustomPicker: UIViewRepresentable {
+    @Binding var dataArrays : [[String]]
+
+    //makeCoordinator()
+    func makeCoordinator() -> CustomPicker.Coordinator {
+        return CustomPicker.Coordinator(self)
+    }
+
+    //makeUIView(context:)
+    func makeUIView(context: UIViewRepresentableContext<CustomPicker>) -> UIPickerView {
+        let picker = UIPickerView(frame: .zero)
+        picker.dataSource = context.coordinator
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    //updateUIView(_:context:)
+    func updateUIView(_ view: UIPickerView, context: UIViewRepresentableContext<CustomPicker>) {
+    }
+
+    class Coordinator: NSObject, UIPickerViewDataSource, UIPickerViewDelegate {
+        var parent: CustomPicker
+        init(_ pickerView: CustomPicker) {
+            self.parent = pickerView
+        }
+
+        //Number Of Components:
+        func numberOfComponents(in pickerView: UIPickerView) -> Int {
+            return parent.dataArrays.count
+        }
+
+        //Number Of Rows In Component:
+        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            return parent.dataArrays[component].count
+        }
+
+        //Width for component:
+        func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+            return UIScreen.main.bounds.width/3
+        }
+
+        //Row height:
+        func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+            return 60
+        }
+
+        //View for Row
+        func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+
+            let view = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width/3, height: 60))
+
+            let pickerLabel = UILabel(frame: view.bounds)
+
+            pickerLabel.text = parent.dataArrays[component][row]
+
+            pickerLabel.adjustsFontSizeToFitWidth = true
+            pickerLabel.textAlignment = .center
+            pickerLabel.lineBreakMode = .byWordWrapping
+            pickerLabel.numberOfLines = 0
+            pickerView.subviews[1].backgroundColor = .clear
+            view.addSubview(pickerLabel)
+
+            view.clipsToBounds = true
+            
+
+            return view
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+            // Access the selected item using the row and component indices
+            let selectedItem = parent.dataArrays[component][row]
+            print("Selected Item: \(selectedItem)")
+            
+            // Update state or perform any action based on the selected item
+            // For example, you can assign the selected item to a @State variable
+            // parent.selectedItem = selectedItem
+        }
+    }
 }
+
