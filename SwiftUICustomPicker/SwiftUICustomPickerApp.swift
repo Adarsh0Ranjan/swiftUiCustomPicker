130b643769a29875812c8c085f9938b3b45b7e91
Adarsh Ranjan <104624542+Adarsh0Ranjan@users.noreply.github.com>
2024-01-17 12:41:27 +0530

    Initial Commit

---
+++ SwiftUICustomPicker/SwiftUICustomPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICustomPickerApp.swift
+//  SwiftUICustomPicker
+//
+//  Created by Adarsh Ranjan on 17/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICustomPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
